b4e0b725a3985a1a85851478e5b79d3e8bb5d853
naimulhuda-official <developernaimulhuda@gmail.com>
2023-12-16 16:37:00 +0600

    updated

--- task-four.php
+++ task-four.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
 // operatore//
 
 01.Arith,etic  operators = [+ , - ,* ,%,/,** ]
@@ -7,3 +16,7 @@
 05.String OPerators = [. , .=]
 06.Array Operators = [+ , == , === , != , <> , !==]
 07.Update Operators =[++ , --]
+
+?>
+</body>
+</html>
